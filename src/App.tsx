98dca85702edf28adb64fc9363f649b88080ead6
armagansenol <armagansnl@gmail.com>
2024-08-18 16:08:19 +0300

    remove page transition wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { Leva } from "leva"
-import { useOutlet } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/home"
+import LetsTalk from "./pages/lets-talk"
+import Works from "./pages/works"
+import DetailWork from "./pages/detail-work"
 
 function App() {
-  const currentOutlet = useOutlet()
-
   return (
     <>
-      {currentOutlet}
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/works" element={<Works />}></Route>
+        <Route path="/works/:projectName" element={<DetailWork />} />
+        <Route path="/lets-talk" element={<LetsTalk />} />
+      </Routes>
       <div className="relative z-[9999999999999999]">
         <Leva />
       </div>
