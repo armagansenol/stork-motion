f5793fc4ff86253a9dad597afae502c7add344f1
armagansenol <armagansnl@gmail.com>
2025-01-28 18:01:21 +0300

    refactor: simplify R3f scroll rig wrapper and adjust scrollbar settings

--- src/hocs/r3f-scroll-rig-wrapper/index.tsx
+++ src/hocs/r3f-scroll-rig-wrapper/index.tsx
@@ -1,6 +1,5 @@
 import { GlobalCanvas, SmoothScrollbar } from "@14islands/r3f-scroll-rig"
-import { useWindowSize } from "@uidotdev/usehooks"
-import { MutableRefObject, ReactNode, useEffect, useRef, useState } from "react"
+import { MutableRefObject, ReactNode, useRef } from "react"
 
 export interface R3fScrollRigWrapperProps {
   children: ReactNode
@@ -8,32 +7,26 @@ export interface R3fScrollRigWrapperProps {
 
 export default function R3fScrollRigWrapper(props: R3fScrollRigWrapperProps) {
   const eventSource = useRef<HTMLDivElement>(null)
-  const [isTouch, setTouch] = useState(false)
-  const size = useWindowSize()
+  // const [isTouch, setTouch] = useState(false)
+  // const size = useWindowSize()
 
-  useEffect(() => {
-    const isTouch = "ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.maxTouchPoints > 0
-    setTouch(isTouch)
+  // useEffect(() => {
+  //   const isTouch = "ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.maxTouchPoints > 0
+  //   setTouch(isTouch)
 
-    console.log("istouch", isTouch)
-  }, [size])
+  //   console.log("istouch", isTouch)
+  // }, [size])
 
   return (
     <>
-      {isTouch ? (
-        <>{props.children}</>
-      ) : (
-        <div ref={eventSource}>
-          <GlobalCanvas
-            eventSource={eventSource as MutableRefObject<HTMLElement>}
-            eventPrefix="client"
-            style={{ pointerEvents: "none" }}
-          />
-          <SmoothScrollbar scrollRestoration="manual">
-            {(bind) => <div {...bind}>{props.children}</div>}
-          </SmoothScrollbar>
-        </div>
-      )}
+      <div ref={eventSource}>
+        <GlobalCanvas
+          eventSource={eventSource as MutableRefObject<HTMLElement>}
+          eventPrefix="client"
+          frameloop="always"
+        />
+        <SmoothScrollbar scrollRestoration="auto">{(bind) => <div {...bind}>{props.children}</div>}</SmoothScrollbar>
+      </div>
     </>
   )
 }
