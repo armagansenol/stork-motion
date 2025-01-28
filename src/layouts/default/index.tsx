2c47353482bbcd5996a47ada2384b067ee51ed97
armagansenol <armagansnl@gmail.com>
2025-01-28 12:25:59 +0300

    Remove unused Lenis scrollbar code in DefaultLayout

--- src/layouts/default/index.tsx
+++ src/layouts/default/index.tsx
@@ -1,17 +1,16 @@
 import { Footer } from "@/components/footer"
-import { useScrollbar } from "@14islands/r3f-scroll-rig"
-import { ReactNode, useEffect } from "react"
+import { ReactNode } from "react"
 
 type Props = {
   children: ReactNode
 }
 
 const DefaultLayout = ({ children }: Props) => {
-  const scrollbar = useScrollbar()
+  // const scrollbar = useScrollbar()
 
-  useEffect(() => {
-    scrollbar.__lenis?.scrollTo(0, { immediate: true })
-  }, [scrollbar])
+  // useEffect(() => {
+  //   scrollbar.__lenis?.scrollTo(0, { immediate: true })
+  // }, [scrollbar])
 
   return (
     <>
